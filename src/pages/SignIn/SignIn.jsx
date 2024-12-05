cd74b21ba717cbc281e37a838597e7e1bd38ceb7
saravanan-prog <53892651+saravanan-prog@users.noreply.github.com>
2024-12-05 22:21:08 +0530

    sign page work

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,6 +1,43 @@
+import { Box, Typography } from "@mui/material"
+
 function SignIn(){
 
-    return <h1>Register</h1>
+    return  <div>
+    <Box
+        sx={{
+            border: '2px solid brown',
+            width: '45%',
+            position:'relative',
+            left:'300px',
+            top:'100px'
+        }}
+    >
+        <div style={{
+            display:'flex',
+            border: '2px solid brown',
+            color: 'brown',
+            backgroundColor: '#f9d188',
+            paddingLeft:'20px'
+
+        }}>
+            <h5>Singn In</h5>
+            <span style={{}}></span>
+            <span style={{}}></span>
+            <span style={{}}></span>
+        </div>
+        <div style={{ height: '300px', padding: '10px' }}>
+            <Typography  variant="h5" sx={{textAlign:'center', color: 'brown'}}> Sign In </Typography>
+
+            
+        </div>
+
+
+
+
+
+    </Box>
+</div>
+    
 }
 
 export default SignIn
--- src/pages/Singup/Singup.jsx
+++ src/pages/Singup/Singup.jsx
@@ -1,6 +1,43 @@
-function Signup(){
+import { Box, Icon, Typography } from "@mui/material"
+import { green } from "@mui/material/colors"
 
-    return <h1>Register</h1>
+function Signup() {
+
+    return <div>
+        <Box
+            sx={{
+                border: '2px solid brown',
+                width: '45%',
+                position:'relative',
+                left:'300px',
+                top:'100px'
+            }}
+        >
+            <div style={{
+                display:'flex',
+                border: '2px solid brown',
+                color: 'brown',
+                backgroundColor: '#f9d188',
+                paddingLeft:'20px'
+
+            }}>
+                <h5>Singn Up</h5>
+                <span style={{}}></span>
+                <span style={{}}></span>
+                <span style={{}}></span>
+            </div>
+            <div style={{ height: '300px', padding: '10px' }}>
+                <Typography  variant="h5" sx={{textAlign:'center', color: 'brown'}}> Sign Up </Typography>
+
+                
+            </div>
+
+
+
+
+
+        </Box>
+    </div>
 }
 
 export default Signup
